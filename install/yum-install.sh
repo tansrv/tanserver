0e784bfee41aedc543bf409bfdd26467cdadf719
Chen Daye <68577787+chen-da-ye@users.noreply.github.com>
2021-06-06 19:25:10 +0800

    Fixed cmake: archive_write_add_filter_zstd.

--- install/yum-install.sh
+++ install/yum-install.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-yum -y install gcc gcc-c++ kernel-devel make cmake && \
-yum -y install openssl-devel postgresql-devel      && \
-chmod +x install.sh                                && \
+yum -y install libarchive gcc gcc-c++ kernel-devel make cmake && \
+yum -y install openssl-devel postgresql-devel                 && \
+chmod +x install.sh                                           && \
 ./install.sh
