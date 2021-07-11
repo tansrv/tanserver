551c3e6aec4bc5adeec5b75a3cb9a9b7e60e6792
chen-da-ye <tanserver@outlook.com>
2021-07-11 20:51:28 +0800

    Update tan_event_select_protocol.cpp

--- src/core/tan_event_select_protocol.cpp
+++ src/core/tan_event_select_protocol.cpp
@@ -10,8 +10,62 @@
 #include "tan_openssl_ssl.h"
 
 
+static void tan_select_protocol(tan_connection_t *conn);
+
+
 void
 tan_event_select_protocol(tan_connection_t *conn)
 {
+    tan_ssl_t      ret;
+    const u_char  *p;
 
+    ret = tan_ssl_readline(conn->info.ssl, conn->event.header);
+
+    switch (ret) {
+
+    case TAN_SSL_READLINE_CONTINUE:
+
+        conn->event.read = tan_event_select_protocol;
+        return;
+
+    case TAN_SSL_READLINE_DONE:
+
+        break;
+
+    case TAN_SSL_READLINE_TOO_LARGE:
+
+        p = tan_get_hostaddr(&conn->info.addr);
+
+        tan_log_info(TAN_REQUEST_HEADER_TOO_LARGE,
+                     p[0], p[1], p[2], p[3]);
+
+    case TAN_SSL_READLINE_FAILED:
+
+        goto out_disconnect;
+    }
+
+    if (tan_unlikely(conn->status.closing & 1))
+        goto out_disconnect;
+
+    tan_select_protocol(conn);
+
+    return;
+
+out_disconnect:
+
+    tan_free_client_connection(conn);
+}
+
+
+static void
+tan_select_protocol(tan_connection_t *conn)
+{
+    const char  *header;
+
+    header = conn->event.header.c_str();
+
+    if (header[0] == 'G' && header[1] == 'E' && header[2] == 'T')
+        tan_event_websocket(conn);
+    else
+        tan_event_custom_protocol(conn);
 }
