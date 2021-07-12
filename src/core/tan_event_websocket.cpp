df9bd74fdfc3653162c980c72e7feedbafb7ca93
chen-da-ye <tanserver@outlook.com>
2021-07-12 13:58:48 +0800

    Update tan_event_websocket.cpp

--- src/core/tan_event_websocket.cpp
+++ src/core/tan_event_websocket.cpp
@@ -14,6 +14,24 @@
 #include "tan_websocket.h"
 
 
+#define TAN_WS_GET_PAYLOAD_LEN_7(buf)                 \
+    (buf[0] & 0x7f)
+
+#define TAN_WS_GET_PAYLOAD_LEN_7_16(buf)              \
+    ((buf[0] & 0xff << 8) | (buf[1] & 0xff))
+
+#define TAN_WS_GET_PAYLOAD_LEN_7_64(buf)              \
+    ((buf[4] & 0xff << 24) | (buf[5] & 0xff << 16) |  \
+        (buf[6] & 0xff << 8) | (buf[7] & 0xff))
+
+
+typedef enum {
+    TAN_WS_READ_HEADER_FAILED = 0,
+    TAN_WS_READ_HEADER_CONTINUE,
+    TAN_WS_READ_HEADER_DONE,
+} tan_ws_read_header_status_e;
+
+
 void
 tan_event_websocket(tan_connection_t *conn)
 {
