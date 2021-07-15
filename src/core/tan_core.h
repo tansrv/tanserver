580e85b35fff0324172c03d4e6e0d34955c2df2c
Chen Daye <68577787+chen-da-ye@users.noreply.github.com>
2021-07-15 23:45:49 +0800

    Update tan_core.h

--- src/core/tan_core.h
+++ src/core/tan_core.h
@@ -19,7 +19,6 @@
 #include "tan_string.h"
 #include "tan_config.h"
 #include "tan_list.h"
-#include "tan_python.h"
 #include "tan_protocol_errors.h"
 #include "tan_process_loop.h"
 #include "tan_syscfg.h"
