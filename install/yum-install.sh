4004f12c79b03faeea39b913979481bf2b00926e
chen-da-ye <tanserver@outlook.com>
2021-07-18 07:56:50 +0800

    Added python3-pip.

--- install/yum-install.sh
+++ install/yum-install.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
-yum -y install libarchive gcc gcc-c++ kernel-devel make cmake python2-devel openssl-devel postgresql-devel && \
-chmod +x install.sh                                                                                        && \
+yum -y install libarchive gcc gcc-c++ kernel-devel make cmake python2-devel python3-pip openssl-devel postgresql-devel && \
+chmod +x install.sh                                                                                                    && \
 ./install.sh
