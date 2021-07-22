47e8aca4166bd53aa079e4137d7a12d343fbbab5
Chen Daye <68577787+chen-da-ye@users.noreply.github.com>
2021-07-23 00:07:38 +0800

    Update apt-install.sh

--- install/apt-install.sh
+++ install/apt-install.sh
@@ -1,11 +1,11 @@
 #! /bin/bash
 
-apt-get update                                                                                 && \
-apt-get -y install build-essential cmake3 python3 python3-dev python3-pip libssl-dev libpq-dev && \
-cd ../src/lib/confuse/build && cmake .. && make && make install                                && \
-cd ../../jsoncpp/build/     && cmake .. && make && make install                                && \
-cd ../../../core/build/     && cmake .. && make && make install                                && \
-cd ../../build/             && cmake .. && make && make install                                && \
+apt-get update                                                                                && \
+apt-get -y install build-essential cmake python3 python3-dev python3-pip libssl-dev libpq-dev && \
+cd ../src/lib/confuse/build && cmake .. && make && make install                               && \
+cd ../../jsoncpp/build/     && cmake .. && make && make install                               && \
+cd ../../../core/build/     && cmake .. && make && make install                               && \
+cd ../../build/             && cmake .. && make && make install                               && \
 pip3 install tanserver
 
 if [ $? -ne 0 ]; then
