ae1d51077c971103d03507c046242c32bf0973f2
chen-da-ye <tanserver@outlook.com>
2021-07-28 11:37:32 +0800

    Update tan_event_ssl.cpp

--- src/core/tan_event_ssl.cpp
+++ src/core/tan_event_ssl.cpp
@@ -11,14 +11,17 @@
 void
 tan_event_ssl_handshake(tan_connection_t *conn)
 {
-    if (tan_ssl_accept(conn->info.ssl)
-        != TAN_SSL_ACCEPT_OK)
-    {
-        return;
-    }
+    if (conn->status.flags & TAN_CONN_STATUS_SSL_HANDSHAKING) {
 
-    /* Handshake succeeded.  */
-    conn->status.flags &= ~TAN_CONN_STATUS_SSL_HANDSHAKING;
+        if (tan_ssl_accept(conn->info.ssl)
+            != TAN_SSL_ACCEPT_OK)
+        {
+            return;
+        } else {
+            /* Handshake succeeded.  */
+            conn->status.flags &= ~TAN_CONN_STATUS_SSL_HANDSHAKING;
+        }
+    }
 
     tan_event_select_protocol(conn);
 }
